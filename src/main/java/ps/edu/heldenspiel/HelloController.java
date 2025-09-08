0577c521d1ccac1255e44c349261e029ddb7ba53
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-09-08 08:48:35 +0200

    Init project

---
+++ src/main/java/ps/edu/heldenspiel/HelloController.java
@@ -0,0 +1,14 @@
+package ps.edu.heldenspiel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
