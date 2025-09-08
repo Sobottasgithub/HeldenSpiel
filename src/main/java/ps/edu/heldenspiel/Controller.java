bc7c6cecb425f0cb96248426e03962d76169f348
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-09-08 08:53:52 +0200

    rename controller and application

--- src/main/java/ps/edu/heldenspiel/Controller.java
+++ src/main/java/ps/edu/heldenspiel/Controller.java
@@ -3,7 +3,7 @@ package ps.edu.heldenspiel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
