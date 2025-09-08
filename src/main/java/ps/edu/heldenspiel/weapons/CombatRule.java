dbf3201a4e976ee014e00737794422e3ff47e831
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-09-08 10:15:24 +0200

    Implement Hero CombatRule and Monster

---
+++ src/main/java/ps/edu/heldenspiel/weapons/CombatRule.java
@@ -0,0 +1,13 @@
+package ps.edu.heldenspiel.weapons;
+import ps.edu.heldenspiel.monster.Monster;
+import ps.edu.heldenspiel.heros.Hero;
+
+public class CombatRule {
+    public CombatRule() {
+
+    }
+
+    public void fight(Hero hero, Monster monster) {
+
+    }
+}
