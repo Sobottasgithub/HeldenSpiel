a1bdf9e73f1cf6ef78b2836011f2c8c481800d29
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-09-17 08:39:29 +0200

    Implement dice

---
+++ src/main/java/ps/edu/heldenspiel/Dice.java
@@ -0,0 +1,24 @@
+package ps.edu.heldenspiel;
+
+import java.util.Random;
+
+public class Dice {
+    private int start;
+    private int end;
+    Random dice = new Random();
+
+    public Dice() {}
+
+    public Dice(int start, int end) {
+        setSides(start, end);
+    }
+
+    public void setSides(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int rollDice() {
+        return this.dice.nextInt(start, end);
+    }
+}
