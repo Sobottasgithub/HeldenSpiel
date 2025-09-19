e6a2bd2e7795f204a93d5fba35d08409ad5f1742
theschenni <scherer.henni@gmail.com>
2025-09-19 14:44:28 +0200

    Added healing ability to Magician

--- src/main/java/ps/edu/heldenspiel/hero/Magician.java
+++ src/main/java/ps/edu/heldenspiel/hero/Magician.java
@@ -2,8 +2,12 @@ package ps.edu.heldenspiel.hero;
 
 import ps.edu.heldenspiel.Weapon;
 
-public class Magician extends Hero{
+public class Magician extends Hero {
     public Magician(String name, int health, int strength, Weapon weapon) {
         super(name, health, strength, weapon);
+    }
+
+    public void healFriendly(Hero friendlyToHeal) {
+        friendlyToHeal.setHealth(friendlyToHeal.getHealth() + weapon.getMagic());
     }
 }
