d7b8dc5edb838ff59db885aaac8017c7aed51a9c
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-10-12 22:55:02 +0200

    Format java source

--- src/main/java/ps/edu/heldenspiel/hero/Archer.java
+++ src/main/java/ps/edu/heldenspiel/hero/Archer.java
@@ -1,36 +1,33 @@
 package ps.edu.heldenspiel.hero;
 
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import ps.edu.heldenspiel.Controller;
 import ps.edu.heldenspiel.utils.Dice;
 import ps.edu.heldenspiel.weapons.Dagger;
 import ps.edu.heldenspiel.weapons.Weapon;
 
 public class Archer extends Hero {
-    Dice d100 = new Dice(0, 100, "d100");
+  Dice d100 = new Dice(0, 100, "d100");
 
-    public Archer() {
-        super("Archer", 20, 10, new Dagger());
-    }
+  public Archer() {
+    super("Archer", 20, 10, new Dagger());
+  }
 
-    public Archer(String name, int health, int strength, Weapon weapon) {
-        super(name, health, strength, weapon);
-    }
+  public Archer(String name, int health, int strength, Weapon weapon) {
+    super(name, health, strength, weapon);
+  }
 
-    @Override
-    public int getAttackDamage() {
-        int damage = weapon.getAttackDamage();
-        int hitPercentage = (int) 3.75 * strength + 25; // uses f(x) = 3.75X+25 as a function
-        if (d100.rollDice() <= hitPercentage) {
-            return damage * 2;
-        }
-        return 0;
+  @Override
+  public int getAttackDamage() {
+    int damage = weapon.getAttackDamage();
+    int hitPercentage = (int) 3.75 * strength + 25; // uses f(x) = 3.75X+25 as a function
+    if (d100.rollDice() <= hitPercentage) {
+      return damage * 2;
     }
+    return 0;
+  }
 
-    public URL getImagePath() {
-        return Controller.class.getResource("images/archer.png");
-    }
+  public URL getImagePath() {
+    return Controller.class.getResource("images/archer.png");
+  }
 }
--- src/main/java/ps/edu/heldenspiel/hero/Priest.java
+++ src/main/java/ps/edu/heldenspiel/hero/Priest.java
@@ -7,25 +7,25 @@ import ps.edu.heldenspiel.weapons.Fist;
 import ps.edu.heldenspiel.weapons.Weapon;
 
 public class Priest extends Hero {
-    Dice d100 = new Dice(0, 100, "d100");
+  Dice d100 = new Dice(0, 100, "d100");
 
-    public Priest() {
-        super("Priest", 20, 10, new Fist());
-    }
+  public Priest() {
+    super("Priest", 20, 10, new Fist());
+  }
 
-    public Priest(String name, int health, int strength, Weapon weapon) {
-        super(name, health, strength, weapon);
-    }
+  public Priest(String name, int health, int strength, Weapon weapon) {
+    super(name, health, strength, weapon);
+  }
 
-    public int divinusInterventu() {
-        if (d100.rollDice() == 100) {
-            return 10000;
-        }
-        return 0;
+  public int divinusInterventu() {
+    if (d100.rollDice() == 100) {
+      return 10000;
     }
+    return 0;
+  }
 
-    @Override
-    public URL getImagePath() {
-        return Controller.class.getResource("images/priest.png");
-    }
+  @Override
+  public URL getImagePath() {
+    return Controller.class.getResource("images/priest.png");
+  }
 }
